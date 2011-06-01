b020f9aaaf934d880e3dae014623a56c7cab7ea4
coco8864@aroma.ocn.ne.jp <coco8864@aroma.ocn.ne.jp@cdfea6d0-a8a3-7231-7b29-4c8a746dfa32>
2011-06-01 12:39:07 +0000

    heart beat監視処理を修正

--- trunk/queuelet-system/src/main/java/naru/queuelet/typed/WatchDeamonQueuelet.java
+++ trunk/queuelet-system/src/main/java/naru/queuelet/typed/WatchDeamonQueuelet.java
@@ -8,6 +8,7 @@ package naru.queuelet.typed;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
@@ -36,17 +37,25 @@ public class WatchDeamonQueuelet implements Queuelet,Runnable {
 	
 	private int type=StartupInfo.TYPE_QUEUELET;
 	private long interval=5000;
+	//起動後、この時間経過すればretryCountをリセットする
+	private int retryResetInterval=60000;
 	
 	private String name;
 	private int restartLimit;
 	private long heartBeatLimit;
 	private String queueletConf;
 	
-	/* 以降には前回の起動情報を格納、異常時再起動に備える */
+	/* 起動以降には前回の起動情報を格納、異常再起動に備える */
 	private String queueletArgs[];
 	private String javaArgs[];
 	private String[] javaVmOptions;
-	private int javaHepSize;
+	private int javaHeapSize;
+	
+	/* 異常再起動する時のオプション */
+	private String recoverQueueletArgs[];
+	private String recoverJavaArgs[];
+	private String[] recoverJavaVmOptions;
+	private int recoverJavaHeapSize;
 
 	/* (非 Javadoc)
 	 * @see naru.queuelet.Queuelet#service(java.lang.Object)
@@ -73,17 +82,49 @@ public class WatchDeamonQueuelet implements Queuelet,Runnable {
 	 */
 	public void init(QueueletContext context, Map param) {
 		this.context=context;
+		String interval=(String)param.get("watch.interval");
+		if(interval!=null){
+			this.interval=Long.parseLong(interval);
+		}
+		String retryResetInterval=(String)param.get("watch.retryResetInterval");
+		if(retryResetInterval!=null){
+			this.retryResetInterval=Integer.parseInt(retryResetInterval);
+		}
+		
 		this.name=(String)param.get("watch.name");
 		String heartBeatLimit=(String)param.get("watch.heartBeatLimit");
 		this.heartBeatLimit=Long.parseLong(heartBeatLimit);
 		String restartLimit=(String)param.get("watch.restartLimit");
 		this.restartLimit=Integer.parseInt(restartLimit);
 		String javaHeapSize=(String)param.get("java.heapSize");
-		this.javaHepSize=Integer.parseInt(javaHeapSize);
+		if(javaHeapSize!=null){
+			this.javaHeapSize=Integer.parseInt(javaHeapSize);
+		}else{
+			this.javaHeapSize=64;//指定がなければ64mとする
+		}
 		this.queueletConf=(String)param.get("queuelet.conf");
 		this.javaVmOptions=arryParam(param,"java.vmOption");
+		if(this.javaVmOptions==null){
+			this.javaVmOptions=new String[0];
+		}
 		this.queueletArgs=arryParam(param,"queuelet.arg");
+		if(this.queueletArgs==null){
+			this.queueletArgs=new String[0];
+		}
 		this.javaArgs=arryParam(param,"java.arg");
+		if(this.javaArgs==null){
+			this.javaArgs=new String[0];
+		}
+		
+		String recoverHeapSize=(String)param.get("java.recoverHeapSize");
+		if(recoverHeapSize!=null){
+			this.recoverJavaHeapSize=Integer.parseInt(recoverHeapSize);
+		}else{
+			this.recoverJavaHeapSize=-1;//指定がなければ前回起動時
+		}
+		this.recoverJavaVmOptions=arryParam(param,"java.recoverVmOption");
+		this.recoverQueueletArgs=arryParam(param,"queuelet.recoverArg");
+		this.recoverJavaArgs=arryParam(param,"java.recoverArg");
 		thread=new Thread(this);
 		thread.start();
 	}
@@ -93,7 +134,6 @@ public class WatchDeamonQueuelet implements Queuelet,Runnable {
 	 */
 	public void term() {
 	}
-	
 	
 	private String getJavaCommand(){
 		String javaHome=System.getenv("JAVA_HOME");
@@ -117,9 +157,10 @@ public class WatchDeamonQueuelet implements Queuelet,Runnable {
 				watchFile.setForceEnd(false);
 				return FORCE_END;
 			}
-			long interval=System.currentTimeMillis()-watchFile.getLastHeartBeat();
-			if(heartBeatLimit>0 && interval>heartBeatLimit){
-				logger.info("hungup:"+name);//音信不通
+			long lastHeartBeat=watchFile.getLastHeartBeat();
+			long heartBeartInterval=System.currentTimeMillis()-lastHeartBeat;
+			if(heartBeatLimit>0 && heartBeatLimit<heartBeartInterval){
+				logger.info("hungup:"+name+":" + new Date(lastHeartBeat) +":"+heartBeartInterval);//音信不通
 				return FORCE_END;
 			}
 			return NOMAL;
@@ -130,7 +171,7 @@ public class WatchDeamonQueuelet implements Queuelet,Runnable {
 				logger.info("retry over:"+name);//音信不通
 				return RETRY_OVER;
 			}
-			logger.info("restart:"+name);//音信不通
+			logger.info("restart:"+name);//再起動
 			return RESTART;
 		}else{
 			return DEMON_STOP;
@@ -153,24 +194,25 @@ public class WatchDeamonQueuelet implements Queuelet,Runnable {
 					  2 + /* -cp ssssssssssss */
 					  1 + /* naru.queuelet.startup.Startup */
 					  1;// conf.xml /* args */
-		String[] vmOption=this.javaVmOptions;
 		if(resStartupInfo!=null&&resStartupInfo.getJavaVmOptions()!=null){
-			this.javaVmOptions=vmOption=resStartupInfo.getJavaVmOptions();
+			javaVmOptions=resStartupInfo.getJavaVmOptions();
+		}else if(this.recoverJavaVmOptions!=null){
+			javaVmOptions=recoverJavaVmOptions;
 		}
-		if(vmOption!=null){
-			cmdLength+=vmOption.length;
-		}
-		int heapSize=this.javaHepSize;
+		cmdLength+=javaVmOptions.length;
+		
 		if(resStartupInfo!=null&&resStartupInfo.getJavaHeapSize()>0){
-			this.javaHepSize=heapSize=resStartupInfo.getJavaHeapSize();
+			javaHeapSize=resStartupInfo.getJavaHeapSize();
+		}else if(recoverJavaHeapSize>0){
+			javaHeapSize=recoverJavaHeapSize;
 		}
-		String[] args=this.queueletArgs;
 		if(resStartupInfo!=null&&resStartupInfo.getArgs()!=null){
-			this.queueletArgs=args=resStartupInfo.getArgs();
+			queueletArgs=resStartupInfo.getArgs();
+		}else if(recoverQueueletArgs!=null){
+			queueletArgs=this.recoverQueueletArgs;
 		}
-		if(args!=null){
-			cmdLength+=args.length;
-		}
+		cmdLength+=queueletArgs.length;
+		
 		//cmdを構築
 		String[] cmd=new String[cmdLength];
 		int pos=0;
@@ -178,13 +220,13 @@ public class WatchDeamonQueuelet implements Queuelet,Runnable {
 		pos++;
 		cmd[pos]="-DQUEUELET_HOME="+ System.getProperty("QUEUELET_HOME");
 		pos++;
-		if(vmOption!=null){
-			System.arraycopy(vmOption, 0, cmd,pos,vmOption.length);
-			pos+=vmOption.length;
+		if(javaVmOptions!=null){
+			System.arraycopy(javaVmOptions, 0, cmd,pos,javaVmOptions.length);
+			pos+=javaVmOptions.length;
 		}
-		cmd[pos]="-Xms" + heapSize +"m";
+		cmd[pos]="-Xms" + javaHeapSize +"m";
 		pos++;
-		cmd[pos]="-Xms" + heapSize +"m";
+		cmd[pos]="-Xms" + javaHeapSize +"m";
 		pos++;
 		cmd[pos]="-cp";
 		pos++;
@@ -194,19 +236,19 @@ public class WatchDeamonQueuelet implements Queuelet,Runnable {
 		pos++;
 		cmd[pos]=queueletConf;
 		pos++;
-		if(args!=null){
-			System.arraycopy(args, 0, cmd,pos,args.length);
-			pos+=args.length;
+		if(queueletArgs!=null){
+			System.arraycopy(queueletArgs, 0, cmd,pos,queueletArgs.length);
+			pos+=queueletArgs.length;
 		}
 		
 		StartupInfo startupInfo=new StartupInfo();//次プロセスに通知するstartupInfo
 		startupInfo.setType(type);
 		startupInfo.setName(name);
-		startupInfo.setArgs(args);
-		startupInfo.setJavaHeapSize(heapSize);
-		startupInfo.setJavaVmOptions(vmOption);
+		startupInfo.setJavaVmOptions(javaVmOptions);
+		startupInfo.setJavaHeapSize(javaHeapSize);
+		startupInfo.setArgs(queueletArgs);
 		
-		logger.info("restartConf:" + watchFile.getStartupInfo().getRestartCount() +" cmd:");
+		logger.info("restartCount:" + watchFile.getStartupInfo().getRestartCount() +" cmd:");
 		for(int i=0;i<cmd.length;i++){
 			logger.info(i +":" +cmd[i]);
 		}
@@ -215,17 +257,23 @@ public class WatchDeamonQueuelet implements Queuelet,Runnable {
 	
 	private void watch(WatchFile watchFile,File stopFile) throws IOException{
 		int retryCount=0;
+		long retryResetTime=0;
 		while(!stopFile.exists()){
 			int result=check(watchFile,retryCount);
 			if(result==FORCE_END){
 				watchFile.terminateChild();
+				retryResetTime=System.currentTimeMillis()+retryResetInterval;
 			}else if(result==RESTART){
 				execChild(watchFile);
 				retryCount++;
 			}else if(result==RETRY_OVER || result==DEMON_STOP){
 				break;
 			}else if(result==NOMAL){
-				retryCount=0;
+				if(retryResetTime>0 && System.currentTimeMillis()>=retryResetTime){
+					logger.info("start success:"+name);
+					retryCount=0;
+					retryResetTime=0;
+				}
 			}
 			try {
 				Thread.sleep(interval);
@@ -240,6 +288,10 @@ public class WatchDeamonQueuelet implements Queuelet,Runnable {
 		File stopFile=WatchFile.getStopFlagFile();
 		try{
 			watchFile=WatchFile.createWatchFile(name);
+			if(watchFile==null){
+				logger.error("fail to createWatchFile.name:"+name);
+				return;//
+			}
 			watch(watchFile,stopFile);
 		} catch (IOException e) {
 			logger.error("watch error.",e);
